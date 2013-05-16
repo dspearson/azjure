8704a5aff0368b239eb446a3429edabb753bab9b
Jason Ozias <jason.g.ozias@gmail.com>
2013-05-16 15:48:38 -0400

    Updated for Clojars push.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject net.ozias.crypt/azjure "0.1.0-SNAPSHOT"
   :description "Encryption Library in Clojure"
-  :url "http://www.ozias.net"
+  :url "https://github.com/CraZySacX/azjure"
   :license {:name "GPLv3"
             :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
@@ -8,5 +8,8 @@
   :aliases {"build" ["install"]
             "sdoc" ["marg" "--multi" "src/" "test/"]}
   :jvm-opts ["-Xmx500m"]
-  :plugins [[lein-marginalia "0.7.1"]]
-  :repositories {"ozias.net" "http://www.ozias.net/archiva"})
+  :plugins [[lein-marginalia "0.7.1"]
+            [lein-clojars "0.9.1"]]
+  :repositories {"ozias.net" "http://www.ozias.net/archiva"}
+  :scm {:name "git"
+         :url "https://github.com/CraZySacX/azjure"})
