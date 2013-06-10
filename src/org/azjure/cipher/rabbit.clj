43c75343b639b597c663d20d1fdc248167e7ec90
Jason Ozias <jason.g.ozias@gmail.com>
2013-06-10 16:57:19 -0400

    Rabbit work.

--- src/org/azjure/cipher/rabbit.clj
+++ src/org/azjure/cipher/rabbit.clj
@@ -21,12 +21,12 @@
 (defn +mod8 [a b]
   (mod (+ a b) 8))
 
-(defn ^{:doc "Adds two 32-bit values mod 2^32 and squares them generating
+(defn- ^{:doc "Adds two 32-bit values mod 2^32 and squares them generating
 a 64-bit result."} square
   [w1 w2]
   (.longValue (bigint (expt (+modw w1 w2) 2))))
 
-(defn gfn ^{:doc "Take two 32-bit values and generate one 32-bit value."} 
+(defn- gfn ^{:doc "Take two 32-bit values and generate one 32-bit value."} 
   [w1 w2]
   (let [sqr (square w1 w2)]
     (bit-xor (bit-and (bit-shift-right sqr 32) 0xFFFFFFFF) 
@@ -53,7 +53,7 @@ a 64-bit result."} square
   (fn [[i0 i1 i2]]
     (+modw (nth g i0) (<<< (nth g i1) 8) (nth g i2))))
 
-(defn next-state [[x c b]]
+(defn- next-state [[x c b]]
   (let [g (reduce (gfn-round x c) [] (range 8))
         evenfn (ns-even g)
         oddfn (ns-odd g)]
@@ -97,12 +97,54 @@ a 64-bit result."} square
 (defn- state-xor [state]
   (reduce state-xor-round state (range 8)))
 
-(defn- mod-counters [state iv]
-  state)
+(defn- xor-counter [c ivec]
+  (fn [[cidx iidx0 iidx1]]
+    (->> (into (nth ivec iidx0) (nth ivec iidx1))
+         (bytes-word)
+         (bit-xor (nth c cidx)))))
 
-(defn- setup-iv [state iv]
-  (if (nil? iv) state (mod-counters state iv)))
+(defn- update-counter [iv]
+  (fn [c round]
+    (let [xfn (xor-counter c iv)]
+      (->> (condp = (mod round 4)
+             0 (xfn [round 1 0])
+             1 (xfn [round 3 1])
+             2 (xfn [round 3 2])
+             3 (xfn [round 2 0]))
+           (assoc c round)))))
 
+(defn- mod-counters [[x c b] iv]
+  (let [ivec (mapv vec (reverse (partition 2 iv)))]
+    [x (reduce (update-counter ivec) c (range 8)) b]))
+
+(defn- ms2b [word]
+  (bit-shift-right word 16))
+
+(defn- ls2b [word]
+  (bit-and word 0xFFFF))
+
+(def roundfn (comp next-state update-counter-system))
+
+(defn- word<-x [x]
+  (fn [[idx0 idx1 idx2 idx3]]
+    (bit-or
+     (bit-xor (ls2b (nth x idx0)) (ms2b (nth x idx1)))
+     (bit-shift-left (bit-xor (ms2b (nth x idx2)) (ls2b (nth x idx3))) 16))))
+
+(defn- rabbit-round [[xi ci bi oi :as state] r]
+  (let [_ (println "Xi: " xi)
+        _ (println "Ci: " ci)
+        _ (println "Bi: " bi)
+        _ (println "Oi: " oi)
+        [xn cn bn] (roundfn [xi ci bi] 0)
+        exfn (word<-x xn)]
+    [xn cn bn
+     (into oi
+           [(exfn [6 3 6 1])
+            (exfn [4 1 4 7])
+            (exfn [2 7 2 5])
+            (exfn [0 5 0 3])])]))
+    
 (defn print-state [state]
   (println "State: " ((juxt #(mapv to-hex (first %)) #(mapv to-hex (second %)) #(to-hex (last %) 1)) state)))
 
@@ -111,13 +153,19 @@ a 64-bit result."} square
 (defrecord Rabbit []
   Cipher
   (initialize [_ {:keys [key iv upper] :or [upper 1024] :as initmap}]
-    (let [ms (-> (comp next-state update-counter-system)
+    (let [ms (-> roundfn
                  (reduce (expand-key key) (range 4))
                  (state-xor))
-          ss (setup-iv ms iv)] 
+          ss (if (nil? iv) ms
+                 (reduce roundfn 
+                         (mod-counters ms iv) (range 4)))] 
       {:ms ms :ss ss}))
   (keysizes-bytes [_] [16])
   StreamCipher
-  (generate-keystream [_ initmap [lower upper :as range]])
+  (generate-keystream [_ {:keys [ss]} [lower upper :as range]]
+    (let [ns (conj ss [])
+          _ (println ns)]
+      (reduce rabbit-round ns [0 1 2])))
+;    (rabbit-round (rabbit-round (conj ss []) 0) 1))
   (keystream-size-bytes [_] max-stream-length-bytes)
   (iv-size-bytes [_] 8))
