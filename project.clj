252ba400f32e56e3cf7d87f00f24a20bea86a77c
Jason Ozias <jason.g.ozias@gmail.com>
2013-06-25 11:30:15 -0400

    Fixed repositories

--- project.clj
+++ project.clj
@@ -13,9 +13,9 @@
             "docs" ["do" "doc," "marg" "--multi" "src/" "test/"]}
   :jvm-opts ["-Xmx500m"]
   :plugins [[codox "0.6.4"]
-            [lein-marginalia "0.7.1"]
-            [lein-clojars "0.9.1"]]
-  :repositories {"ozias.net" "http://www.ozias.net/archiva"}
+            [lein-marginalia "0.7.1"]]
+  :repositories [["snapshots" {:url "http://www.ozias.net/archiva/repository/snapshots/" :creds :gpg}]
+                 ["releases" {:url "http://www.ozias.net/archiva/repository/internal/" :creds :gpg}]]
   :scm {:name "git"
          :url "https://github.com/CraZySacX/azjure"}
   :codox {:output-dir "api"
