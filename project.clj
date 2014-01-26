4df5d0e94e79a1c40d70e8f5089f5da0e0677f19
Jason Ozias <jason.g.ozias@gmail.com>
2014-01-25 20:36:56 -0500

    deps update, docs update

--- project.clj
+++ project.clj
@@ -4,24 +4,26 @@
   :license {:name "GPLv3"
             :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/math.numeric-tower "0.0.2"]]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.3"]
-                                  [com.taoensso/timbre "2.1.2"]]}}
-  :target-path "target/"
-  :aliases {"build" ["install"]
-            "docs" ["do" "doc," "marg" "--multi" "src/" "test/"]
-            "deployall" ["do" "deploy," "deploy" "clojars"]}
-  :jvm-opts ["-Xmx500m"]
-  :plugins [[codox "0.6.4"]
-            [lein-marginalia "0.7.1"]]
+                   :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                                  [com.taoensso/timbre "3.0.0-RC4"]]
+                   :plugins [[codox "0.6.6"]]}}
+  :aliases {"package" ["do" "clean," "uberjar"]
+            "most" ["do" "clean," "doc," "package"]
+            "dep" ["do" "deploy," "deploy" "clojars"]
+            "all" ["do" "most," "dep"]}
+  :jvm-opts ["-Xms1024m" "-Xmx1024m"]
   :deploy-repositories [["snapshots" 
-                         {:url "http://www.ozias.net/archiva/repository/snapshots/"
+                         {:url "http://www.ozias.net/artifactory/libs-snapshot-local"
                           :creds :gpg}]
                         ["releases"
-                         {:url "http://www.ozias.net/archiva/repository/internal/"
+                         {:url "http://www.ozias.net/artifactory/libs-release-local"
                           :creds :gpg}]]
   :scm {:name "git"
          :url "https://github.com/CraZySacX/azjure"}
   :codox {:output-dir "api"
-          :sources ["src" "test"]})
+          :exclude [leiningen.version]
+          :sources ["src" "test"]
+          :src-dir-uri "http://github.com/CraZySacX/overbuild/blob/master/"
+          :src-linenum-anchor-prefix "L"})
