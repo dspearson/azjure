da82508b300690ae9e176aa6694ff287cb37cf3b
Jason Ozias <jason.g.ozias@gmail.com>
2014-02-07 10:15:22 -0500

    version bumps

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [com.taoensso/timbre "3.0.0-RC4"]]
+                 [com.taoensso/timbre "3.0.0"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]]
                    :plugins [[codox "0.6.6"]]}
