9ef2030c07ba75a7fd91a63bb1baa2b614eed85e
Jason Ozias <jason.g.ozias@gmail.com>
2014-05-20 16:38:08 -0400

    docs work

--- src/azjure/core.clj
+++ src/azjure/core.clj
@@ -1,8 +1,9 @@
-;; [azjure]: https://github.com/CraZySacX/azjure
+;; [azjure]: https://crazysacx.github.io/azjure
 ;; [cipher]: azjure.cipher.cipher.html
 ;; [modes]: azjure.modes.html
 ;; [pad]: azjure.padders.html
 ;; [encoders]: azjure.encoders.html
+
 (ns azjure.core
   "Core Azjure API for encryption/decryption.
 
@@ -38,12 +39,12 @@
   [azjure.padders] [pad] for supported values.
   * **eid** - A keyword that represents the encryption input decoder you wish to
   use. See [azjure.encoders] [encoders] for supported values.
-  * **eoe** - A keyword that represents the encryption output encoder you wish to
-  use. See [azjure.encoders] [encoders] for supported values.
+  * **eoe** - A keyword that represents the encryption output encoder you wish
+  to use. See [azjure.encoders] [encoders] for supported values.
   * **did** - A keyword that represents the decryption input decoder you wish to
   use. See [azjure.encoders] [encoders] for supported values.
-  * **doe** - A keyword that represents the decryption output encoder you wish to
-  use. See [azjure.encoders] [encoders] for supported values.
+  * **doe** - A keyword that represents the decryption output encoder you wish
+  to use. See [azjure.encoders] [encoders] for supported values.
   * **key** - A vector of unsigned bytes (0-255) of the appropriate length that
   represents the key you wish to use with the cipher.
   * **iv** - A vector of unsigned bytes (0-255) of the appropriate length that
